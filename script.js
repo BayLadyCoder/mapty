c49b223707a7fd87ff84b3e2bf129e1f28ff1a39
BayC <rachada_32@hotmail.com>
2023-01-24 00:45:33 -0500

    Use Leaflet to render map with user's geolocation

--- script.js
+++ script.js
@@ -18,7 +18,27 @@ if (navigator.geolocation) {
 
       const { latitude, longitude } = position.coords;
 
-      console.log(`https://www.google.pt/maps/@${latitude},${longitude}`);
+      // console.log(`https://www.google.pt/maps/@${latitude},${longitude}`);
+
+      const coords = [latitude, longitude];
+
+      const zoomLevel = 13;
+
+      // 'map' must be the same name with an `id` in an HTML element for rendering map in HTML
+      const map = L.map('map').setView(coords, zoomLevel);
+
+      // tile layer is where you can change map theme
+      // for more theme, see: https://leaflet-extras.github.io/leaflet-providers/preview/
+      L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+
+      // marker and popup when click on a specific location in the map
+      L.marker(coords)
+        .addTo(map)
+        .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+        .openPopup();
     },
     (err) => console.log('err', err)
   );
