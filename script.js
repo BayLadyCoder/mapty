3855afb3775639ec302a668756e551d6f42ff27b
BayC <rachada_32@hotmail.com>
2023-02-20 17:19:13 -0500

    Create calcPace method in Running class, and create calcSpeed method in Cycling class

--- script.js
+++ script.js
@@ -28,6 +28,13 @@ class Running extends Workout {
   constructor(coords, distance, duration, cadence) {
     super(coords, distance, duration);
     this.cadence = cadence;
+    this.calcPace();
+  }
+
+  calcPace() {
+    // min/km
+    this.pace = this.duration / this.distance;
+    return this.pace;
   }
 }
 
@@ -35,6 +42,13 @@ class Cycling extends Workout {
   constructor(coords, distance, duration, elevationGain) {
     super(coords, distance, duration);
     this.elevationGain = elevationGain;
+    this.calcSpeed();
+  }
+
+  calcSpeed() {
+    // km/h
+    this.speed = this.distance / (this.duration / 60);
+    return this.speed;
   }
 }
 
