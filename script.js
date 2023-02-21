f2f36a98b503bbad4e7f215c1ed15eb2b155919c
BayC <rachada_32@hotmail.com>
2023-02-20 22:06:29 -0500

    Add the 2nd part of html in _renderWorkout()

--- script.js
+++ script.js
@@ -207,7 +207,7 @@ class App {
   }
 
   _renderWorkout(workout) {
-    const html = `
+    let html = `
     <li class="workout workout--${workout.type}" data-id="${workout.id}">
           <h2 class="workout__title">${workout.description}</h2>
           <div class="workout__details">
@@ -223,6 +223,36 @@ class App {
             <span class="workout__unit">min</span>
           </div>
     `;
+
+    if (workout.type === 'running') {
+      html += `
+       <div class="workout__details">
+            <span class="workout__icon">⚡️</span>
+            <span class="workout__value">${workout.pace.toFixed(1)}</span>
+            <span class="workout__unit">min/km</span>
+          </div>
+          <div class="workout__details">
+            <span class="workout__icon">🦶🏼</span>
+            <span class="workout__value">${workout.cadence}</span>
+            <span class="workout__unit">spm</span>
+          </div>
+        </li>
+      `;
+    } else {
+      html += `
+       <div class="workout__details">
+            <span class="workout__icon">⚡️</span>
+            <span class="workout__value">${workout.speed.toFixed(1)}</span>
+            <span class="workout__unit">km/h</span>
+          </div>
+          <div class="workout__details">
+            <span class="workout__icon">⛰</span>
+            <span class="workout__value">${workout.elevationGain}</span>
+            <span class="workout__unit">m</span>
+          </div>
+        </li> -->
+      `;
+    }
   }
 }
 
